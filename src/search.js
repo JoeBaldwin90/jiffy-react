390fecfeebddad5142717153af7e2da85fee9072
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-09-11 19:26:42 +0100

    Add search section and input

---
+++ src/search.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Search = () => (
+    <div className="search grid">
+        <input className="input grid-item" placeholder="type something"></input>
+    </div>
+);
+
+export default Search;
