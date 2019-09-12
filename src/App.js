e57df6c1f421f118cdf537b5f18f30a88d4bcba1
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-09-12 15:19:27 +0100

    Create function to modify Giphy API endpoint based on the user's search term, then send a GET requestest

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Header from "./header";
-import UserHint from "./user-hint"
+import UserHint from "./user-hint";
 
 class App extends Component {
   constructor(props) {
@@ -11,12 +11,30 @@ class App extends Component {
     };
   }
 
+  searchGiphy = inputValue => {
+    fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=Is7P13H7y3yqxijMkU17rfd24X4abCPC&q=${inputValue}&limit=20&offset=0&rating=PG&lang=en`
+    )
+      .then(response => response.json())
+      .then(data => {
+        const gifs = [];
+        data.data.map(item => {
+          gifs.push(item.images.original.mp4);
+        });
+        console.log(gifs);
+      })
+      .catch(error => {
+        alert("Search failed. Try entering a different search term");
+      });
+  };
+
   handleChange = event => {
     const inputValue = event.target.value;
     this.setState((prevState, props) => ({
       ...prevState,
       searchTerm: inputValue,
-      hintText: inputValue.length > 2 ? `Hit enter to search for ${inputValue}` : ""
+      hintText:
+        inputValue.length > 2 ? `Hit enter to search for ${inputValue}` : ""
     }));
   };
 
@@ -24,6 +42,7 @@ class App extends Component {
     const inputValue = event.target.value;
     if (inputValue.length > 2 && event.key === "Enter") {
       console.log("Searching for " + inputValue);
+      this.searchGiphy(inputValue);
     }
   };
 
