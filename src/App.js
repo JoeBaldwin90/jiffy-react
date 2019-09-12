78fed71e722b38e4406bf888ae7a735e909bff76
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-09-12 12:48:06 +0100

    Add basic functions to search input to handle input change and do something once 'Enter' has been pressed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import Header from './header';
-import Search from './search';
+import React, { Component } from "react";
+import Header from "./header";
 
-function App() {
-  return (
-    <div className="page">
-      <Header />
-      <Search />
-    </div>
-  );
+class App extends Component {
+  
+  handleChange = event => {
+    const { value } = event.target
+    if (value.length > 2) {
+      console.log(value);
+    }
+  } 
+
+  handleKeyPress = event => {
+    const { value } = event.target;
+    if (value.length > 2 && event.key === 'Enter') {
+      console.log("search for " + value);
+    }
+  }
+
+  render() {
+    return (
+      <div className="page">
+        <Header />
+
+        <div className="search grid">
+          <input
+            className="input grid-item"
+            placeholder="type something"
+            onChange={this.handleChange}
+            onKeyPress={this.handleKeyPress}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/search.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Search = () => (
-    <div className="search grid">
-        <input className="input grid-item" placeholder="type something"></input>
-    </div>
-);
-
-export default Search;
